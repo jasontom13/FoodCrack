112488879e8aa11500275d6f538c6e5bb425ac9d
MissKelly <woodti@email.arizona.edu>
2015-04-04 18:06:55 -0700

    Singleton
    Set up singleton for DatabaseHelper

--- src/edu/arizona/wood/tom/DatabaseHelper.java
+++ src/edu/arizona/wood/tom/DatabaseHelper.java
@@ -1,5 +1,25 @@
 package edu.arizona.wood.tom;
 
 public class DatabaseHelper {
+	private static DatabaseHelper defaultInstance;
 	
+	private DatabaseHelper()
+	{
+		init();
+	}
+	
+	private void init()
+	{
+		
+	}
+	
+	public static DatabaseHelper getDefaultInstance()
+	{
+		if (defaultInstance == null)
+		{
+			defaultInstance = new DatabaseHelper();
+		}
+		
+		return defaultInstance;
+	}
 }
