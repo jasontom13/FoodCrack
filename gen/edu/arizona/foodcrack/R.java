13d1972d5f53b0b403327a2d512bc4c01b058a18
Jason Tom <jtom77@gmail.com>
2015-04-07 13:07:12 -0700

    Main Activity Menu Added

--- gen/edu/arizona/foodcrack/R.java
+++ gen/edu/arizona/foodcrack/R.java
@@ -15,19 +15,26 @@ public final class R {
         public static final int fancy_edit_off=0x7f020001;
         public static final int fancy_edit_on=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int toggleselector=0x7f020004;
+        public static final int icon=0x7f020004;
+        public static final int iconsmall=0x7f020005;
+        public static final int iconwhite=0x7f020006;
+        public static final int toggleselector=0x7f020007;
     }
     public static final class id {
-        public static final int loginButton=0x7f060004;
-        public static final int nameField=0x7f060001;
-        public static final int nameText=0x7f060000;
-        public static final int passField=0x7f060003;
-        public static final int passText=0x7f060002;
+        public static final int action_settings=0x7f070005;
+        public static final int loginButton=0x7f070004;
+        public static final int nameField=0x7f070001;
+        public static final int nameText=0x7f070000;
+        public static final int passField=0x7f070003;
+        public static final int passText=0x7f070002;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_splash=0x7f030002;
+    }
+    public static final class menu {
+        public static final int sample=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/edu/arizona/wood/tom/MainActivity.java
+++ src/edu/arizona/wood/tom/MainActivity.java
@@ -1,27 +1,36 @@
 package edu.arizona.wood.tom;
 
+import edu.arizona.wood.tom.FlyOutContainer;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
+import android.view.Menu;
+import android.view.View;
 import edu.arizona.foodcrack.R;
 
 public class MainActivity extends Activity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	FlyOutContainer root;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		this.root = (FlyOutContainer) this.getLayoutInflater().inflate(R.layout.activity_main, null);
+		
+		this.setContentView(root);
+	}
 
-        setContentView(R.layout.activity_main);
-
-
-//        mTabHost.addTab(mTabHost.newTabSpec("simple").setIndicator("Simple"),
-//                FragmentStackSupport.CountingFragment.class, null);
-//        mTabHost.addTab(mTabHost.newTabSpec("contacts").setIndicator("Contacts"),
-//                LoaderCursorSupport.CursorLoaderListFragment.class, null);
-//        mTabHost.addTab(mTabHost.newTabSpec("custom").setIndicator("Custom"),
-//                LoaderCustomSupport.AppListFragment.class, null);
-//        mTabHost.addTab(mTabHost.newTabSpec("throttle").setIndicator("Throttle"),
-//                LoaderThrottleSupport.ThrottledLoaderListFragment.class, null);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sample, menu);
+		return true;
+	}
+	
+	public void toggleMenu(View v){
+		this.root.toggleMenu();
+	}
 }
