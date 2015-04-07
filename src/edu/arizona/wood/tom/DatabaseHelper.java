020e01a659c8ba932caa7bb3ef025bc323c2b1a5
MissKelly <woodti@email.arizona.edu>
2015-04-06 21:55:54 -0700

    AWS SDK
    AWS SDK files added

--- src/edu/arizona/wood/tom/DatabaseHelper.java
+++ src/edu/arizona/wood/tom/DatabaseHelper.java
@@ -1,7 +1,12 @@
 package edu.arizona.wood.tom;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+
 public class DatabaseHelper {
 	private static DatabaseHelper defaultInstance;
+	private static final String ACCESS_KEY = "AKIAI4CSISLJMKU3DWVQ";
+	private static final String SECRET_KEY = "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";
 	
 	private DatabaseHelper()
 	{
@@ -10,7 +15,7 @@ public class DatabaseHelper {
 	
 	private void init()
 	{
-		
+		AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
 	}
 	
 	public static DatabaseHelper getDefaultInstance()
