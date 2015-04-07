0b58b26e91220b6e6d8eee418cc4f2f8e926bb79
Jason Tom <jtom77@gmail.com>
2015-04-07 13:45:43 -0700

    Slight Changes

--- gen/edu/arizona/foodcrack/R.java
+++ gen/edu/arizona/foodcrack/R.java
@@ -12,16 +12,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int fancy_edit_off=0x7f020001;
-        public static final int fancy_edit_on=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int iconsmall=0x7f020005;
-        public static final int iconwhite=0x7f020006;
-        public static final int toggleselector=0x7f020007;
+        public static final int button_selector=0x7f020001;
+        public static final int fancy_edit_off=0x7f020002;
+        public static final int fancy_edit_on=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int iconsmall=0x7f020006;
+        public static final int iconwhite=0x7f020007;
+        public static final int toggleselector=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f070005;
+        public static final int action_settings=0x7f070006;
+        public static final int button1=0x7f070005;
         public static final int loginButton=0x7f070004;
         public static final int nameField=0x7f070001;
         public static final int nameText=0x7f070000;
