51762e6b3bb4684989093363f02b568c58ab7cf9
Jason Tom <jtom77@gmail.com>
2015-04-09 15:28:07 -0700

    Idk

--- gen/edu/arizona/foodcrack/R.java
+++ gen/edu/arizona/foodcrack/R.java
@@ -11,29 +11,37 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int background=0x7f020000;
-        public static final int button_selector=0x7f020001;
-        public static final int fancy_edit_off=0x7f020002;
-        public static final int fancy_edit_on=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int icon=0x7f020005;
-        public static final int iconsmall=0x7f020006;
-        public static final int iconwhite=0x7f020007;
-        public static final int toggleselector=0x7f020008;
+        public static final int applelogo=0x7f020000;
+        public static final int background=0x7f020001;
+        public static final int button_selector=0x7f020002;
+        public static final int fancy_edit_off=0x7f020003;
+        public static final int fancy_edit_on=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int iconsmall=0x7f020007;
+        public static final int iconwhite=0x7f020008;
+        public static final int toggleselector=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f070006;
-        public static final int button1=0x7f070005;
-        public static final int loginButton=0x7f070004;
-        public static final int nameField=0x7f070001;
-        public static final int nameText=0x7f070000;
-        public static final int passField=0x7f070003;
-        public static final int passText=0x7f070002;
+        public static final int action_settings=0x7f07000c;
+        public static final int foodImage=0x7f070000;
+        public static final int loginButton=0x7f07000a;
+        public static final int nameField=0x7f070007;
+        public static final int nameText=0x7f070006;
+        public static final int newGameButton=0x7f07000b;
+        public static final int passField=0x7f070009;
+        public static final int passText=0x7f070008;
+        public static final int selection1=0x7f070002;
+        public static final int selection2=0x7f070003;
+        public static final int selection3=0x7f070004;
+        public static final int selection4=0x7f070005;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_splash=0x7f030002;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_splash=0x7f030003;
     }
     public static final class menu {
         public static final int sample=0x7f060000;
